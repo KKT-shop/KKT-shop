9c126485dec52b15ef5fd2145cfc47a60bef086e
ThachSacKda <Kdatsgcc210007@fpt.edu.vn>
2023-02-22 21:54:00 +0700

    showproduct

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
+
+/**
+ * @Route("/product")
+ */ 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+
+    //ADMIN
+    private ProductRepository $repo;
+    public function __construct(ProductRepository $repo)
     {
+        $this->repo = $repo;
+    }
+    /**
+     * @Route("/productt", name="product_show")
+     */
+    public function readAllAction(): Response
+    {
+        $products = $this->repo->findAll();
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products
         ]);
     }
 }
