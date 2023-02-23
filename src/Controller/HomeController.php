0257563eb83fca3c2c812ab369c0ac7b66e7c26a
Bui Tran Phuong Thao <121841993+thaobtpgcc210031@users.noreply.github.com>
2023-02-23 10:45:37 +0700

    update category ctl

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+public function homepage(CategoryRepository $repo, CategoryRepository $brand): Response
+        {   $br= $brand->findAll();
+            $pro= $repo->findAll();
+            return $this->render('home/index.html.twig', [
+                'pro'=> $pro,
+                'brand'=>$br
+            ]);
     }
+
+   
 }
---
+++ src/Entity/Product.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $namep;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $pricep;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNamep(): ?string
+    {
+        return $this->namep;
+    }
+
+    public function setNamep(string $namep): self
+    {
+        $this->namep = $namep;
+
+        return $this;
+    }
+
+    public function getPricep(): ?float
+    {
+        return $this->pricep;
+    }
+
+    public function setPricep(float $pricep): self
+    {
+        $this->pricep = $pricep;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
